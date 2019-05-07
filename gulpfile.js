5806a630801b907e4ef8233573a0a415e4439397
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-05-08 00:31:07 +0530

    styles task tweaked.

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('styles', function () {
-    return gulp.src('./scss/styles.scss')
+    return gulp.src('./scss/*.scss')
         .pipe(wait(250))
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
