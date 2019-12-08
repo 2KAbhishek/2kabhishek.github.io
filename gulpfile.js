0786f7458f477165130a7e5b6f6ff2019efe0165
Abhishek Keshri <keshri.abhishek63@gmail.com>
2019-12-08 21:03:02 +0530

    Beautify files.

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,11 @@ var wait = require('gulp-wait');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 // var imagemin = require('gulp-imagemin');
-const { series } = require('gulp');
+const {
+    series
+} = require('gulp');
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
     return gulp.src('js/scripts.js')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
@@ -21,14 +23,18 @@ gulp.task('scripts', function() {
                 comments: '/^!/'
             }
         }))
-        .pipe(rename({extname: '.min.js'}))
+        .pipe(rename({
+            extname: '.min.js'
+        }))
         .pipe(gulp.dest('js'));
 });
 
 gulp.task('styles', function () {
     return gulp.src('./scss/*.scss')
         .pipe(wait(250))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
 
@@ -39,7 +45,7 @@ gulp.task('styles', function () {
 //     .pipe(gulp.dest('min'))
 // );
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('js/scripts.js', gulp.series('scripts'));
     gulp.watch('scss/styles.scss', gulp.series('styles'));
     gulp.watch('scss/styles-dark.scss', gulp.series('styles'));
