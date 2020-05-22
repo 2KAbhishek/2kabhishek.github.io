a1a5507749248d4cb9603af60eef0fa3ebdaf717
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-22 09:09:04 +0530

    Fix formatting for scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,93 +1,108 @@
 (function ($) {
-
     // Remove no-js class
-    $('html').removeClass('no-js');
+    $("html").removeClass("no-js");
 
     // Animate to section when nav is clicked
-    $('header a').click(function (e) {
-
+    $("header a").click(function (e) {
         // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+        if ($(this).hasClass("no-scroll")) return;
 
         e.preventDefault();
-        var heading = $(this).attr('href');
+        var heading = $(this).attr("href");
         var scrollDistance = $(heading).offset().top;
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        $("html, body").animate(
+            {
+                scrollTop: scrollDistance + "px",
+            },
+            Math.abs(window.pageYOffset - $(heading).offset().top) / 1
+        );
 
         // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
+        if ($("header").hasClass("active")) {
+            $("header, body").removeClass("active");
         }
     });
 
     // Scroll to top
-    $('#to-top').click(function () {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
+    $("#to-top").click(function () {
+        $("html, body").animate(
+            {
+                scrollTop: 0,
+            },
+            500
+        );
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function () {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 500);
+    $("#lead-down span").click(function () {
+        var scrollDistance = $("#lead").next().offset().top;
+        $("html, body").animate(
+            {
+                scrollTop: scrollDistance + "px",
+            },
+            500
+        );
     });
 
     // Create timeline
-    $('#experience-timeline').each(function () {
-
+    $("#experience-timeline").each(function () {
         $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+        $userContent = $this.children("div"); // user content
 
         // Create each timeline block
         $userContent.each(function () {
-            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+            $(this)
+                .addClass("vtimeline-content")
+                .wrap(
+                    '<div class="vtimeline-point"><div class="vtimeline-block"></div></div>'
+                );
         });
 
         // Add icons to each block
-        $this.find('.vtimeline-point').each(function () {
-            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        $this.find(".vtimeline-point").each(function () {
+            $(this).prepend(
+                '<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>'
+            );
         });
 
         // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function () {
-            var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
+        $this.find(".vtimeline-content").each(function () {
+            var date = $(this).data("date");
+            if (date) {
+                // Prepend if exists
+                $(this)
+                    .parent()
+                    .prepend(
+                        '<span class="vtimeline-date">' + date + "</span>"
+                    );
             }
         });
-
     });
 
     // Open mobile menu
-    $('#mobile-menu-open').click(function () {
-        $('header, body').addClass('active');
+    $("#mobile-menu-open").click(function () {
+        $("header, body").addClass("active");
     });
 
     // Close mobile menu
-    $('#mobile-menu-close').click(function () {
-        $('header, body').removeClass('active');
+    $("#mobile-menu-close").click(function () {
+        $("header, body").removeClass("active");
     });
 
     // Load additional projects
-    $('#view-more-projects').click(function (e) {
+    $("#view-more-projects").click(function (e) {
         e.preventDefault();
         $(this).fadeOut(300, function () {
-            $('#more-projects').fadeIn(300);
+            $("#more-projects").fadeIn(300);
         });
     });
 
     // Change Theme
-    $('#changeTheme').click(function () {
-        light = 'css/styles.css';
-        dark = 'css/styles-dark.css';
-        theme = ($('#css').attr('href')) == light ? dark : light;
-        $('#css').attr('href', theme);
+    $("#changeTheme").click(function () {
+        light = "css/styles.css";
+        dark = "css/styles-dark.css";
+        theme = $("#css").attr("href") == light ? dark : light;
+        $("#css").attr("href", theme);
     });
-
 })(jQuery);
