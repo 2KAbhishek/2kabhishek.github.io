6b6a01ff2a7c2b9bcdf549eb07b11bc976536b5b
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-05-22 09:06:57 +0530

    Fix formatting of gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,51 @@
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var wait = require('gulp-wait');
-var rename = require('gulp-rename');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
+var wait = require("gulp-wait");
+var rename = require("gulp-rename");
+var autoprefixer = require("gulp-autoprefixer");
 // var imagemin = require('gulp-imagemin');
-const {
-    series
-} = require('gulp');
+const { series } = require("gulp");
 
-gulp.task('scripts', function () {
-    return gulp.src('js/scripts.js')
-        .pipe(plumber(plumber({
-            errorHandler: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        })))
-        .pipe(uglify({
-            output: {
-                comments: '/^!/'
-            }
-        }))
-        .pipe(rename({
-            extname: '.min.js'
-        }))
-        .pipe(gulp.dest('js'));
+gulp.task("scripts", function () {
+    return gulp
+        .src("js/scripts.js")
+        .pipe(
+            plumber(
+                plumber({
+                    errorHandler: function (err) {
+                        console.log(err);
+                        this.emit("end");
+                    },
+                })
+            )
+        )
+        .pipe(
+            uglify({
+                output: {
+                    comments: "/^!/",
+                },
+            })
+        )
+        .pipe(
+            rename({
+                extname: ".min.js",
+            })
+        )
+        .pipe(gulp.dest("js"));
 });
 
-gulp.task('styles', function () {
-    return gulp.src('./scss/*.scss')
+gulp.task("styles", function () {
+    return gulp
+        .src("./scss/*.scss")
         .pipe(wait(250))
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(
+            sass({
+                outputStyle: "compressed",
+            }).on("error", sass.logError)
+        )
+        .pipe(gulp.dest("./css"));
 });
 
 // Optimize Images
@@ -45,10 +55,10 @@ gulp.task('styles', function () {
 //     .pipe(gulp.dest('min'))
 // );
 
-gulp.task('watch', function () {
-    gulp.watch('js/scripts.js', gulp.series('scripts'));
-    gulp.watch('scss/styles.scss', gulp.series('styles'));
-    gulp.watch('scss/styles-dark.scss', gulp.series('styles'));
+gulp.task("watch", function () {
+    gulp.watch("js/scripts.js", gulp.series("scripts"));
+    gulp.watch("scss/styles.scss", gulp.series("styles"));
+    gulp.watch("scss/styles-dark.scss", gulp.series("styles"));
 });
 
-exports.default = series('styles', 'scripts');
+exports.default = series("styles", "scripts");
