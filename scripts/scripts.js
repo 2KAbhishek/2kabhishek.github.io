2d0a45e5bb626bfe7b8e9007f9b0f872f54cb64c
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-08-13 03:51:06 +0530

    Update formatting

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,7 @@ var TxtType = function (el, toRotate, period) {
     this.el = el;
     this.loopNum = 0;
     this.period = parseInt(period, 10) || 2000;
-    this.txt = "";
+    this.txt = '';
     this.tick();
     this.isDeleting = false;
 };
@@ -18,7 +18,7 @@ TxtType.prototype.tick = function () {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
 
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
     var that = this;
     var delta = 150 - Math.random() * 100;
@@ -30,7 +30,7 @@ TxtType.prototype.tick = function () {
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === "") {
+    } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
         delta = 500;
@@ -42,50 +42,50 @@ TxtType.prototype.tick = function () {
 };
 
 window.onload = function () {
-    var elements = document.getElementsByClassName("typewriter");
+    var elements = document.getElementsByClassName('typewriter');
     for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute("data-type");
-        var period = elements[i].getAttribute("data-period");
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
         if (toRotate) {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
     // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewriter > .wrap { border-right: 0.1em solid #1688f0}";
+    var css = document.createElement('style');
+    css.type = 'text/css';
+    css.innerHTML = '.typewriter > .wrap { border-right: 0.1em solid #1688f0}';
     document.body.appendChild(css);
 };
 
 (function ($) {
     // Remove no-js class
-    $("html").removeClass("no-js");
+    $('html').removeClass('no-js');
 
     // Animate to section when nav is clicked
-    $("header a").click(function (e) {
+    $('header a').click(function (e) {
         // Treat as normal link if no-scroll class
-        if ($(this).hasClass("no-scroll")) return;
+        if ($(this).hasClass('no-scroll')) return;
 
         e.preventDefault();
-        var heading = $(this).attr("href");
+        var heading = $(this).attr('href');
         var scrollDistance = $(heading).offset().top;
 
-        $("html, body").animate(
+        $('html, body').animate(
             {
-                scrollTop: scrollDistance + "px",
+                scrollTop: scrollDistance + 'px',
             },
             Math.abs(window.pageYOffset - $(heading).offset().top) / 1
         );
 
         // Hide the menu once clicked if mobile
-        if ($("header").hasClass("active")) {
-            $("header, body").removeClass("active");
+        if ($('header').hasClass('active')) {
+            $('header, body').removeClass('active');
         }
     });
 
     // Scroll to top
-    $("#to-top").click(function () {
-        $("html, body").animate(
+    $('#to-top').click(function () {
+        $('html, body').animate(
             {
                 scrollTop: 0,
             },
@@ -94,58 +94,58 @@ window.onload = function () {
     });
 
     // Scroll to first element
-    $("#lead-down span").click(function () {
-        var scrollDistance = $("#lead").next().offset().top;
-        $("html, body").animate(
+    $('#lead-down span').click(function () {
+        var scrollDistance = $('#lead').next().offset().top;
+        $('html, body').animate(
             {
-                scrollTop: scrollDistance + "px",
+                scrollTop: scrollDistance + 'px',
             },
             500
         );
     });
 
     // Create timeline
-    $("#experience-timeline").each(function () {
+    $('#experience-timeline').each(function () {
         $this = $(this); // Store reference to this
-        $userContent = $this.children("div"); // user content
+        $userContent = $this.children('div'); // user content
 
         // Create each timeline block
         $userContent.each(function () {
             $(this)
-                .addClass("vtimeline-content")
+                .addClass('vtimeline-content')
                 .wrap(
                     '<div class="vtimeline-point"><div class="vtimeline-block"></div></div>'
                 );
         });
 
         // Add icons to each block
-        $this.find(".vtimeline-point").each(function () {
+        $this.find('.vtimeline-point').each(function () {
             $(this).prepend(
                 '<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>'
             );
         });
 
         // Add dates to the timeline if exists
-        $this.find(".vtimeline-content").each(function () {
-            var date = $(this).data("date");
+        $this.find('.vtimeline-content').each(function () {
+            var date = $(this).data('date');
             if (date) {
                 // Prepend if exists
                 $(this)
                     .parent()
                     .prepend(
-                        '<span class="vtimeline-date">' + date + "</span>"
+                        '<span class="vtimeline-date">' + date + '</span>'
                     );
             }
         });
     });
 
     // Open mobile menu
-    $("#mobile-menu-open").click(function () {
-        $("header, body").addClass("active");
+    $('#mobile-menu-open').click(function () {
+        $('header, body').addClass('active');
     });
 
     // Close mobile menu
-    $("#mobile-menu-close").click(function () {
-        $("header, body").removeClass("active");
+    $('#mobile-menu-close').click(function () {
+        $('header, body').removeClass('active');
     });
 })(jQuery);
