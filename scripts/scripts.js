aca94e95fe9fc44d7897859fdea3b8426b1cbdfa
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-06-29 11:31:09 +0530

    feat: switch to no-build setup
    - Deletes all build-related files and dependencies.
    - Converts the SCSS file to a plain, human-readable CSS file.
    - Updates index.html to use the non-minified JavaScript file.
    - Optimizes the CSS by removing unused comments, standardizing color formats, and simplifying rules.
    - Modernizes the JavaScript by converting to an ES6 class, using let/const, and adopting modern DOM manipulation and event handling.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,78 +1,78 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
+'use strict';
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-    var that = this;
-    var delta = 150 - Math.random() * 100;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+class TxtType {
+    constructor(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
         this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
+        this.tick();
     }
 
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
+    tick() {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewriter');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtType(elements[i], JSON.parse(toRotate), period);
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
+
+        this.el.innerHTML = `<span class="wrap">${this.txt}</span>`;
+
+        let delta = 150 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(() => this.tick(), delta);
     }
+}
+
+window.addEventListener('load', () => {
+    const elements = document.querySelectorAll('.typewriter');
+    elements.forEach((element) => {
+        const toRotate = element.getAttribute('data-type');
+        const period = element.getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(element, JSON.parse(toRotate), period);
+        }
+    });
+
     // INJECT CSS
-    var css = document.createElement('style');
+    const css = document.createElement('style');
     css.type = 'text/css';
     css.innerHTML = '.typewriter > .wrap { border-right: 0.1em solid #1688f0}';
     document.body.appendChild(css);
-};
+});
 
-(function($) {
-    // Remove no-js class
+(function ($) {
     $('html').removeClass('no-js');
 
     // Animate to section when nav is clicked
-    $('header a').click(function(e) {
-        // Treat as normal link if no-scroll class
+    $('header a').on('click', function (e) {
         if ($(this).hasClass('no-scroll')) return;
 
         e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+        const heading = $(this).attr('href');
+        const scrollDistance = $(heading).offset().top;
 
         $('html, body').animate(
             {
-                scrollTop: scrollDistance + 'px'
+                scrollTop: `${scrollDistance}px`
             },
             Math.abs(window.pageYOffset - $(heading).offset().top) / 1
         );
@@ -84,33 +84,23 @@ window.onload = function() {
     });
 
     // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate(
-            {
-                scrollTop: 0
-            },
-            500
-        );
+    $('#to-top').on('click', () => {
+        $('html, body').animate({scrollTop: 0}, 500);
     });
 
     // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate(
-            {
-                scrollTop: scrollDistance + 'px'
-            },
-            500
-        );
+    $('#lead-down span').on('click', () => {
+        const scrollDistance = $('#lead').next().offset().top;
+        $('html, body').animate({scrollTop: `${scrollDistance}px`}, 500);
     });
 
     // Create timeline
-    $('#experience-timeline').each(function() {
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+    $('#experience-timeline').each(function () {
+        const $this = $(this);
+        const $userContent = $this.children('div');
 
         // Create each timeline block
-        $userContent.each(function() {
+        $userContent.each(function () {
             $(this)
                 .addClass('vtimeline-content')
                 .wrap(
@@ -119,33 +109,28 @@ window.onload = function() {
         });
 
         // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
+        $this.find('.vtimeline-point').each(function () {
             $(this).prepend(
                 '<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>'
             );
         });
 
         // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
-            var date = $(this).data('date');
+        $this.find('.vtimeline-content').each(function () {
+            const date = $(this).data('date');
             if (date) {
-                // Prepend if exists
                 $(this)
                     .parent()
-                    .prepend(
-                        '<span class="vtimeline-date">' + date + '</span>'
-                    );
+                    .prepend(`<span class="vtimeline-date">${date}</span>`);
             }
         });
     });
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
+    $('#mobile-menu-open').on('click', () => {
         $('header, body').addClass('active');
     });
 
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
+    $('#mobile-menu-close').on('click', () => {
         $('header, body').removeClass('active');
     });
 })(jQuery);
